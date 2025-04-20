523347029066ed2907460a8c5dde7c71b46f0b94
liamtgiles17 <liamthomasgiles1702@gmail.com>
2025-04-20 04:53:07 +0100

    deploy to gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
-
+const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     devIndicators: false,
+    reactStrictMode: true,
+    images: {
+      unoptimized: true,
+    },
+    assetPrefix: isProd ? '/the-two-ways/' : '',
+    basePath: isProd ? '/the-two-ways' : '',
+    output: 'export'
 };
 
 export default withFlowbiteReact(nextConfig);
